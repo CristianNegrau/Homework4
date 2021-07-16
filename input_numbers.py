3e34c6c41025ceca938a3c5e56a69dd8515ad3bc
Cristian Negrau <cristian.negrau@protonmail.com>
2021-07-16 21:18:29 +0300

    updated second exercise

--- input_numbers.py
+++ input_numbers.py
@@ -1,10 +1,8 @@
 # Scrieti un program in care user-ul trebuie sa introduca un numar intre 50 si 100.
 # Daca introduce alt numar, printati "Your number is below 50" sau "Your number is above 100".
 # Daca introduce litere, printati "You should enter a number"
-import string
 
 user = input('Insert a number from 50 to 100:\n')
-
 
 try:
   if int(user) < 50:
